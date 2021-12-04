b76755785126619bb285598252e5ba89cdc7b13a
johntelforduk <jtelford1@gmail.com>
2021-12-04 12:51:45 +0000

    Refactor board grid.

--- 04-giant-squid/solution4.py
+++ 04-giant-squid/solution4.py
@@ -5,81 +5,72 @@ class Board:
 
     def __init__(self, numbers: []):
         self.completed = False
+
+        # def cartesian_to_grid(x, y): return y * 5 + x
+        # so...
+        #   grid[0] is top left corner.
+        #   grid[4] is top right corner.
+        #   grid[12] is centre.
+        #   grid[24] is bottom right.
         self.grid = {}
 
-        i = 0
+        g = 0
         for row in numbers:
             num_list = row.split(' ')
             while len(num_list) > 0:
                 possible_num = num_list.pop(0)
                 if possible_num != '':                          # Sometimes there are double spaces between numbers.
-                    self.grid[i % 5, i // 5] = possible_num
-                    i += 1
+                    self.grid[g] = possible_num
+                    g += 1
 
     def is_winner(self) -> bool:
         """Returns True if the board has either a complete row or a complete column."""
         for i in range(5):
             row_fail, col_fail = False, False           # Assume both row & column about to be checked are winners.
             for j in range(5):
-                if self.grid[i, j] != '*':
+                if self.grid[i * 5 + j] != '*':
                     col_fail = True
-                if self.grid[j, i] != '*':
+                if self.grid[i + j * 5] != '*':
                     row_fail = True
             if not row_fail or not col_fail:
                 self.completed = True
                 return True
         return False
 
-    def cell_to_coords(self, i: int) -> ():
-        return i % 5, i // 5
-
-    def get_cell(self, i: int) -> str:
-        return self.grid[self.cell_to_coords(i)]
-
     def mark(self, drawn_number: str):
         """Mark off the parm drawn number on the board by setting it to a star."""
-        for i in range(25):
-            if self.get_cell(i) == drawn_number:
-                self.grid[i % 5, i // 5] = '*'
+        for g in self.grid:
+            if self.grid[g] == drawn_number:
+                self.grid[g] = '*'
 
     def sum_unmarked(self) -> int:
         """Return the sum of the non-star numbers on the grid."""
         total = 0
-        for i in range(25):
-            if self.get_cell(i) != '*':
-                total += int(self.get_cell(i))
+        for g in self.grid:
+            if self.grid[g] != '*':
+                total += int(self.grid[g])
         return total
+
 
 f = open('input.txt')
 t = f.read()
 f.close()
 r = t.split('\n')
 
-# print(r)
-
-selections = r.pop(0).split(',')
-print(selections)
-
-# print()
-# print(r)
-# print()
+selections = r.pop(0).split(',')                # List of number selections, as list if strings.
 
 boards = []
 while len(r) > 0:
-    r.pop(0)            # Discard blank line.
+    r.pop(0)                                    # Discard the blank line between each board.
 
     five_rows = []
     while len(five_rows) < 5:
         five_rows.append(r.pop(0))
 
-    # print(five_rows)
-    # new_board = Board(five_rows)
-    # print(new_board.grid[(0, 0)], new_board.grid[(4, 4)])
     boards.append(Board(five_rows))
 
 winner_count = 0
-
-while winner_count < len(boards):             # Loop until boards completed.
+while winner_count < len(boards):               # Loop until all boards completed.
     drawn = selections.pop(0)
     for b in boards:
         if not b.completed:
@@ -88,4 +79,3 @@ while winner_count < len(boards):             # Loop until boards completed.
                 winner_count += 1
                 if winner_count == 1 or winner_count == len(boards):
                     print(b.sum_unmarked() * int(drawn))
-
