ffbf79738afb17ddaa8e7075c8bcfa13a1b396e6
johntelforduk <jtelford1@gmail.com>
2021-12-05 14:40:28 +0000

    Solution to day 5.

--- 05-hydrothermal-venture/solution5.py
+++ 05-hydrothermal-venture/solution5.py
@@ -1,16 +1,18 @@
 # Solution to day 5 of AOC 2021, Hydrothermal Venture
 # https://adventofcode.com/2021/day/5
 
+import pygame
+
 f = open('input.txt')
 t = f.read()
 f.close()
 
 
-def increment(x: int) -> int:
+def increment(num: int) -> int:
     """Return the delta (0, 1, or -1) that is needed to get from zero to the parm number in single unit increments."""
-    if x < 0:
+    if num < 0:
         return -1
-    elif x > 0:
+    elif num > 0:
         return 1
     else:
         return 0
@@ -20,18 +22,14 @@ def increment(x: int) -> int:
 # v = number of lines crossing that coordinate.
 seabed = {}
 
-# for row in t.split('\n'):
-#     xxx = row.split(' -> ')
-#     print(xxx)
-#
-
 for row in t.split('\n'):
     terms = row.split(' -> ')
     x1, y1 = tuple([int(cart) for cart in terms[0].split(',')])
     x2, y2 = tuple([int(cart) for cart in terms[1].split(',')])
 
-    # For now, only consider horizontal and vertical lines.
+    # Part 1: For now, only consider horizontal and vertical lines.
     # if x1 == x2 or y1 == y2:
+
     xd = increment(x2 - x1)
     yd = increment(y2 - y1)
 
@@ -61,9 +59,31 @@ for y in range(10):
     print()
 print()
 
-count = 0
+count, max_vents, max_x, max_y = 0, 0, 0, 0
 for (x, y) in seabed:
     if seabed[(x, y)] >= 2:
         count += 1
+    max_vents = max(max_vents, seabed[(x, y)])
+    max_x = max(max_x, x)
+    max_y = max(max_y, y)
 
-print(count)
+print(count, max_vents, max_x, max_y)
+
+screen_size = [max_x + 1, max_y + 1]  # [width, height]
+
+pygame.init()                                               # Initialize the game engine.
+screen = pygame.display.set_mode(screen_size)
+
+background_colour = (255, 255, 255)
+screen.fill(background_colour)
+
+for (x, y) in seabed:
+    intensity = 255 - 255 * seabed[(x, y)] // max_vents   # Normalise the colour intensity.
+    colour = (intensity, intensity, intensity)
+    screen.set_at((x, y), colour)
+
+pygame.display.flip()
+
+pygame.image.save(screen, 'screenshots/day5_2.jpg')
+
+pygame.quit()
--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 Solutions to Advent of Code 2021 puzzles.
 
 ## Installation
+```commandline
+pip install pygame
+```
+## Day 5: Hydrothermal Venture
+Visualisation of part 2.
+
+![Day 5, part 2](./05-hydrothermal-venture/screenshots/day5_2.jpg)
