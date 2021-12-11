528551cacb5cca3b61e13abad860fa502d40928a
johntelforduk <jtelford1@gmail.com>
2021-12-11 17:24:28 +0000

    Solution to day 10.

--- 10-syntax-scoring/solution10.py
+++ 10-syntax-scoring/solution10.py
@@ -1,13 +1,16 @@
 # Solution to day 10 of AOC 2021, Syntax Scoring
 # https://adventofcode.com/2021/day/10
 
+from statistics import median
+
+pairs = {'(': ')',
+         '[': ']',
+         '{': '}',
+         '<': '>'}
+
+
 def parse_chunk(line: str) -> str:
     global error_score
-
-    pairs = {'(': ')',
-             '[': ']',
-             '{': '}',
-             '<': '>'}
 
     if len(line) >= 2:
         if line[0] in pairs:
@@ -21,19 +24,17 @@ def parse_chunk(line: str) -> str:
             if the_rest[0] == partner:
                 return parse_chunk(the_rest[1:])
 
-            errors =  {')': 3,
-                       ']': 57,
-                       '}': 1197,
-                       '>': 25137}
+            errors = {')': 3,
+                      ']': 57,
+                      '}': 1197,
+                      '>': 25137}
 
             if the_rest[0] in errors:
                 # print(the_rest[0])
                 error_score = errors[the_rest[0]]
 
-
-#    print('incomplete:', line)
-
     return line
+
 
 assert parse_chunk('') == ''
 
@@ -59,21 +60,44 @@ error_score = 0
 assert parse_chunk('<{([([[(<>()){}]>(<<{{') != ''
 assert error_score == 25137
 
-# error_score = 0
-# output =  parse_chunk('[({(<(())[]>[[{[]{<()<>>')
-# print(output)
+incomplete_score = {')': 1,
+                    ']': 2,
+                    '}': 3,
+                    '>': 4}
 
-
-
-
-f = open('test.txt')
+f = open('input.txt')
 t = f.read()
 f.close()
 
-total = 0
-for l in t.split('\n'):
-    error_score = 0
-    if parse_chunk(l) != '':
+total, incomplete_scores = 0, []
+for row in t.split('\n'):
+    error_score, to_close = 0, []
+
+    if parse_chunk(row) != '':
         total += error_score
 
+    if error_score == 0:
+        for c in row:
+            if c in pairs:
+                to_close.insert(0, pairs[c])
+
+            elif c in ')]}>':
+                i = 0
+                remove_needed = True
+                while i <= len(to_close) - 1 and remove_needed:
+                    if to_close[i] == c:
+                        to_close.pop(i)
+                        remove_needed = False
+                    i += 1
+
+    print(row, error_score, to_close)
+
+    this_incomplete = 0
+    while len(to_close) > 0:
+        this_incomplete *= 5
+        this_incomplete += incomplete_score[to_close.pop(0)]
+    if this_incomplete > 0:
+        incomplete_scores.append(this_incomplete)
+
 print(total)
+print(median(incomplete_scores))
